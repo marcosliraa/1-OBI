11088ab479deb7a32c0faf1c92ab57bd6663f1dd
José Marcos de Lira Júnior <jmlj.lira@gmail.com>
2016-07-11 22:24:55 -0300

    Update exercicio.c
    Adicionada a entrada a função processo.

--- exercicio.c
+++ exercicio.c
@@ -1,4 +1,33 @@
 /*Programa da OBI*/
 
-#include stdio.h
-#include stdlib.h
+#include <stdio.h>
+#include <stdlib.h>
+
+int processo(void);
+
+int main(void)
+{
+  int resposta;
+  resposta = processo();
+  printf("%d\n",resposta);
+}
+
+int processo(void)
+{
+  int r, i, *x0, *xf, *y0, *yf;
+  printf("Digite o numero de redes: \n");
+  scanf("%d", &r);  
+  x0=calloc(r,sizeof(int));
+  xf=calloc(r,sizeof(int));
+  y0=calloc(r,sizeof(int));
+  xf=calloc(r,sizeof(int));
+  i=0;
+  while(x0 != NULL)
+  {
+    printf("Digite a Variacao de X seguida da variacao de Y");
+    scanf("%d,%d,%d,%d",x0[i],xf[i],y0[i],yf[i]);
+  }
+ 
+ 
+  
+}
