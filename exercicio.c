479476b9a08938f7aa6a1c09a2f265688df95ea5
José Marcos de Lira Júnior <jmlj.lira@gmail.com>
2016-07-13 21:29:57 -0300

    exercicio.c
    Adicionada as restrições das variáveis

--- exercicio.c
+++ exercicio.c
@@ -17,6 +17,12 @@ int processo(void)
   int r, i, *x0, *xf, *y0, *yf;
   printf("Digite o numero de redes: \n");
   scanf("%d", &r);  
+  
+  if(r<1 || r>100)
+  {
+    printf("Error, try again.\n");
+    return EXIT_FAILURE;
+  }
  
   x0=calloc(r,sizeof(int));
   xf=calloc(r,sizeof(int));
@@ -26,12 +32,21 @@ int processo(void)
   i=0;
   while(i<r)
   {
+    printf("Digite o valo de x0 da rede %d\n",i+1);
+    scanf("%d",&x0[i]);
     printf("Digite o valo de xf da rede %d\n",i+1);
     scanf("%d",&xf[i]);
     printf("Digite o valo de y0 da rede %d\n",i+1);
     scanf("%d",&y0[i]);
     printf("Digite o valo de yf da rede %d\n",i+1);
     scanf("%i",&yf[i]);
+    if((x0<1 || x0>100) || (xf<1 || xf>100) || (y0<1 || y0>100) || (yf<1 || yf>100))
+    {
+        printf("Error, try again.\n");
+        return EXIT_FAILURE;
+    }
+    i++;
+  }
    
 
-}
+  }
