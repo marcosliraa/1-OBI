68eda14ac12808cd28c2131af7b91c15a3b50bee
José Marcos de Lira Júnior <jmlj.lira@gmail.com>
2016-07-28 15:38:18 -0300

    Adicionado a função de processo ao tesouro.c

--- tesouro.c
+++ tesouro.c
@@ -33,4 +33,63 @@ int main()
     processo();
     return 0;
 }
+int processo(void)
+{
+    int n,k,i;
+    dis_p K[MAX];
 
+    do
+    {
+        printf("Digite o a dimensao do quadriculado:\n");
+        scanf("%d",&n);
+        if(n<2 || n>100)
+            printf("Erro, digite o valor correto da dimensao!\n\n");
+    } while(n<2 || n>100);
+
+    do
+    {
+        printf("Digite o numero de pistas:\n");
+        scanf("%d",&k);
+        if(k<1 || k>100)
+            printf("Erro, digite o valor correto das pistas!\n");
+    } while(k<1 || k>100);
+
+    for(i=0;i<k;i++)
+    {
+        printf("Digite x da dica %d:\n",i+1);
+        scanf("%d",&K[i].p.x);
+        printf("Digite y da dica %d:\n",i+1);
+        scanf("%d",&K[i].p.y);
+        printf("Digite a distancia ao tesouro da dica %d:\n",i+1);
+        scanf("%d",&K[i].d);
+
+    }
+    int x, y;
+    bool test = false;
+    point_p resposta;
+    for(x=0;x<n;x++)
+    {
+        for(y=0;y<n;y++)
+        {
+            point_p pt = {x,y};
+            for(i=0;i<k;i++)
+            {
+                if(dist(pt,K[i].p) != K[i].d)
+                    break;
+            }
+            if(i==k)
+            {
+                if(test)
+                    goto resp;
+                test = true;
+                resposta = pt;
+
+            }
+        }
+    }
+    printf("\n%d %d\n",resposta.x,resposta.y);
+    return 0;
+resp:
+    printf("-1 -1\n");
+    return 0;
+}
